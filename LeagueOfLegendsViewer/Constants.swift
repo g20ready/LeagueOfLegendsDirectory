e42f5a83a064b067408c189b8e2c6ba763bdc50d
g20ready <tmarselg@gmail.com>
2016-10-22 18:49:52 +0300

    Initial commit

---
+++ LeagueOfLegendsViewer/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  LeagueOfLegendsViewer
+//
+//  Created by Marsel Xhaxho on 22/10/2016.
+//  Copyright © 2016 max@dev. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct LOLApiDetails {
+        
+    }
+    
+}
---
+++ Pods/Target Support Files/Pods-LeagueOfLegendsViewerUITests/Pods-LeagueOfLegendsViewerUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_LeagueOfLegendsViewerUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LeagueOfLegendsViewerUITestsVersionString[];
+
